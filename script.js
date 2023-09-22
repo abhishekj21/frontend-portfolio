81ce31113576b161e75302f54eb23bf94269627a
unknown <singhabhishekj21@gmail.com>
2023-09-22 20:37:34 +0100

    I'm working on github section

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hii");
+const boxContainer = document.querySelector(".boxContainer");
+
+for (let i = 0; i < 365; i++) {
+  const el = document.createElement("div");
+  el.classList = "box";
+  boxContainer.appendChild(el);
+}
